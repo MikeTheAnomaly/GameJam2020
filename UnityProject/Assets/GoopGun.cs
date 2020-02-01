b75513c7b85b7d466d41a937dc1e754f9cbdd152
Michael Hoff <46758667+MikeTheAnomaly@users.noreply.github.com>
2020-02-01 17:55:49 -0600

    d

--- UnityProject/Assets/GoopGun.cs
+++ UnityProject/Assets/GoopGun.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class GoopGun : MonoBehaviour
 {
     public GameObject GoopProjectile;
+    public GameObject GoopBounceProjectile;
     public Transform FirePostion;
 
-    public int numberToSpawn;
+    public int ballNumToSpawn;
+    public int ballBounceNum;
     Queue<GameObject> balls;
+    Queue<GameObject> ballsbounce;
     // Start is called before the first frame update
     void Start()
     {
         balls = new Queue<GameObject>();
-        for (int i = 0; i < numberToSpawn; i++)
+        for (int i = 0; i < ballNumToSpawn; i++)
         {
             GameObject ball = Instantiate(GoopProjectile);
             ball.SetActive(false);
             balls.Enqueue(ball);
         }
-        
 
+        ballsbounce = new Queue<GameObject>();
+        for (int i = 0; i < ballBounceNum; i++)
+        {
+            GameObject ball = Instantiate(GoopBounceProjectile);
+            ball.SetActive(false);
+            ballsbounce.Enqueue(ball);
+        }
     }
 
     // Update is called once per frame
@@ -46,8 +55,24 @@ public class GoopGun : MonoBehaviour
 
     }
 
+    public void FireBounce(bool fliped)
+    {
+        Quaternion roation;
+        if (!fliped)
+        {
+            roation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + 180);
+        }
+        else
+        {
+            roation = this.transform.rotation;
+        }
+        
+        ballsbounce.Enqueue(respawn(ballsbounce.Dequeue(), FirePostion.position, roation));
+    }
+
     private GameObject respawn(GameObject obj, Vector3 position, Quaternion roation)
     {
+        obj.GetComponent<GoopShoot>().InstantRemove();
         obj.transform.position = position;
         obj.transform.rotation = roation;
         obj.SetActive(true);
--- UnityProject/Assets/GoopShoot.cs
+++ UnityProject/Assets/GoopShoot.cs
@@ -7,7 +7,9 @@ public class GoopShoot : MonoBehaviour
     public float shootForce;
     Rigidbody2D rb;
 
+    public float sizeIncrease = 10;
     private bool connected = false;
+    private bool wait = false;
     // Start is called before the first frame update
     void OnEnable()
     {
@@ -20,21 +22,33 @@ public class GoopShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(connected && transform.localScale.x <= sizeIncrease)
+        {
+            transform.localScale = transform.localScale + new Vector3(10 * Time.deltaTime,10 * Time.deltaTime,0);
+            wait = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag != "Player" && !connected)
+        if (( collision.gameObject.tag != "blobbounce" && collision.gameObject.tag != "Player") && !connected)
         {
             connected = true;
             rb.constraints = RigidbodyConstraints2D.FreezeAll;
-            transform.localScale = new Vector3(10, 10);
+            //transform.localScale = new Vector3(10, 10);
+
+            if (collision.gameObject.tag == "blob" && transform.localScale.x <= sizeIncrease)
+            {
+                wait = true;
+                transform.localScale = new Vector3(.8f * sizeIncrease, .8f * sizeIncrease, 0);
+            }
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "blob")
+        if(collision.gameObject.tag == "blob" && !wait)
         {
             
             RemoveBlob();   
@@ -50,6 +64,14 @@ public class GoopShoot : MonoBehaviour
     IEnumerator Fade()
     {
         yield return new WaitForSeconds(1f);
+        connected = false;
+        transform.localScale = new Vector2(1, 1);
+        this.gameObject.SetActive(false);
+    }
+
+    public void InstantRemove()
+    {
+        rb.constraints = RigidbodyConstraints2D.None;
         connected = false;
         transform.localScale = new Vector2(1, 1);
         this.gameObject.SetActive(false);
