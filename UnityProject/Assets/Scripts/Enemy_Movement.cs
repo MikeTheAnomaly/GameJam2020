0089048343431bd7cab68bb253b0a6741dc42382
Michael Hoff <46758667+MikeTheAnomaly@users.noreply.github.com>
2020-01-31 20:22:26 -0600

    Enemy_Movements
    movements of the enemy

---
+++ UnityProject/Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    public LayerMask enemyMask;
+    public float speed = 1;
+    Rigidbody2D myBody;
+    Transform myTrans;
+    float myWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTrans = this.transform;
+        myBody = this.GetComponent<Rigidbody2D>();
+        myWidth = this.GetComponentInParent<SpriteRenderer>().bounds.extents.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check to see if there's ground in front of it before moving forward
+        Vector2 lineCastPos = myTrans.position - myTrans.right * myWidth;
+        Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down,Color.red);
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, enemyMask);
+        bool isBlocked = Physics2D.Linecast(lineCastPos, lineCastPos - myTrans.right, enemyMask);
+        
+
+        if (!isGrounded)
+        {
+            Vector3 currRot = myTrans.eulerAngles;
+            currRot.y += 180;
+            myTrans.eulerAngles = currRot;
+        }
+
+        //Always move forward
+        Vector2 myVel = myBody.velocity;
+        myVel.x = -myTrans.right.x * speed;
+        myBody.velocity = myVel;
+    }
+}
