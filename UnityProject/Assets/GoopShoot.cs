91b76852dd2e3bf9ce316693b25cdf245808a257
Michael Hoff <46758667+MikeTheAnomaly@users.noreply.github.com>
2020-02-01 16:21:54 -0600

    goo

--- UnityProject/Assets/GoopShoot.cs
+++ UnityProject/Assets/GoopShoot.cs
@@ -9,11 +9,13 @@ public class GoopShoot : MonoBehaviour
 
     private bool connected = false;
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
         rb = this.GetComponent<Rigidbody2D>();
         rb.AddForce(transform.right * shootForce);
     }
+    
+    
 
     // Update is called once per frame
     void Update()
@@ -47,8 +49,11 @@ public class GoopShoot : MonoBehaviour
 
     IEnumerator Fade()
     {
-            yield return new WaitForSeconds(1f);
-
+        yield return new WaitForSeconds(1f);
+        connected = false;
+        transform.localScale = new Vector2(1, 1);
         this.gameObject.SetActive(false);
     }
+
+    
 }
