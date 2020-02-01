8e8a5bf6bd5d44f12d95291c3c8b4c018e01605d
Michael Hoff <46758667+MikeTheAnomaly@users.noreply.github.com>
2020-02-01 02:46:46 -0600

    movment

---
+++ UnityProject/Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D CC;
+    public float speed;
+    public bool canMove = true;
+
+    public Controllable controller;
+
+    private bool inputX;
+    public bool inputA;
+    private Vector3 joy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(CC == null)
+        {
+            this.GetComponent<CharacterController2D>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        joy = controller.Joystick1();
+        inputX = controller.InputX();
+        inputA = controller.InputA();
+    }
+
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            CC.Move(joy.x *  speed * Time.fixedDeltaTime, inputX, inputA);
+        }
+    }
+}
