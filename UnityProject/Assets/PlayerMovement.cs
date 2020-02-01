552c9a3e2864ad7432c3958787ac222a44b964f6
Michael Hoff <46758667+MikeTheAnomaly@users.noreply.github.com>
2020-02-01 05:58:36 -0600

    wow

--- UnityProject/Assets/PlayerMovement.cs
+++ UnityProject/Assets/PlayerMovement.cs
@@ -30,7 +30,17 @@ public class PlayerMovement : MonoBehaviour
         inputX = controller.InputX();
         inputA = controller.InputA();
         Debug.Log(Input.GetAxis("Mouse Y"));
-        arm.rotation =Quaternion.Euler(0, 0,Input.GetAxis("Mouse Y"));
+        //arm.rotation =Quaternion.Euler(0, 0,Input.GetAxis("Mouse Y"));
+
+        Vector3 mouse = Input.mousePosition;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, .25f) * Mathf.Rad2Deg; ;
+        if (!CC.m_FacingRight)
+        {
+            angle *= -1;
+        }
+        arm.transform.rotation = Quaternion.Euler(0, 0,Mathf.Clamp(angle,-80,90));
     }
 
     private void FixedUpdate()
