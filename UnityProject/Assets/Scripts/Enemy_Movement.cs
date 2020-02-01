b2c3cad05574007e271bd4e4ae65c17d47ef994d
MaxSchaad <46759510+MaxSchaad@users.noreply.github.com>
2020-02-01 03:09:42 -0600

    fixedddddedd micheal breaking things
    dg

--- UnityProject/Assets/Scripts/Enemy_Movement.cs
+++ UnityProject/Assets/Scripts/Enemy_Movement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class Enemy_Movement : MonoBehaviour
 {
     public LayerMask enemyMask;
     public float speed = 1;
