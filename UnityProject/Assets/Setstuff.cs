deca3c7d5bb2938939dd8faaa374a64680089942
Michael Hoff <46758667+MikeTheAnomaly@users.noreply.github.com>
2020-02-02 23:24:48 -0600

    wow

---
+++ UnityProject/Assets/Setstuff.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setstuff : MonoBehaviour
+{
+    public Sprite[] elem;
+    public  TextAsset[] textelems;
+    public  int elemCurrent = 0;
+    public  int nextScene = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StaticStoryStuff.elem = this.elem;
+        StaticStoryStuff.textelems = this.textelems;
+        StaticStoryStuff.elemCurrent = this.elemCurrent;
+        StaticStoryStuff.nextScene = this.elemCurrent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
